46f844ac498f8900c5c63ae26c3dc045d5ac53f2
Mayank SIngh Bhati <mayankbhati324@gmail.com>
2026-03-03 12:52:26 +0530

    Some Changes to add demo

--- app.py
+++ app.py
@@ -184,3 +184,10 @@ if __name__ == '__main__':
     print("="*70 + "\n")
 
     app.run(debug=True, host='127.0.0.1', port=5000, use_reloader=False)
+
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
